141a691bf43ddf0e61bb5ee26e66f94ec352ce47
Alex Banakas <abanakas@gmail.com>
2017-05-09 08:36:03 -0400

    removed obj folder from repo

--- jsSite/obj/Release/AspnetCompileMerge/TempBuildDir/dateToday.js
+++
@@ -1,9 +0,0 @@
-﻿var today = new Date();
-var d = today.getDate();
-var m = today.getMonth() + 1; //January is 0
-var yyyy = today.getFullYear();
-
-var today = d + '/' + m + '/' + yyyy;
-
-document.write(today);
-
