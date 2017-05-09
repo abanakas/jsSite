7f4a045f2f89e3e950d336c2d6377101efe3e929
Alex Banakas <abanakas@gmail.com>
2017-05-08 22:01:31 -0400

    create repo

---
+++ jsSite/obj/Release/AspnetCompileMerge/Source/dateToday.js
@@ -0,0 +1,9 @@
+﻿var today = new Date();
+var d = today.getDate();
+var m = today.getMonth() + 1; //January is 0
+var yyyy = today.getFullYear();
+
+var today = d + '/' + m + '/' + yyyy;
+
+document.write(today);
+
